08631253312a33f3ec4311e505a0dbc0517cbe2e
Tallon Simmons <tallon.simmons@icloud.com>
2022-06-14 18:57:41 -0400

    Register handlers and data infrastructure

---
+++ VacoBuiltCodeTest.CompositionRoot/AppComposition.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using VacoBuiltCodeTest.Application.Services.Requests;
+using VacoBuiltCodeTest.Core.Contracts;
+using VacoBuiltCodeTest.Core.DataModels;
+using VacoBuiltCodeTest.Infrastructure;
+using VacoBuiltCodeTest.Infrastructure.Repositories;
+
+namespace VacoBuiltCodeTest.CompositionRoot
+{
+    public static class AppComposition
+    {
+        public static void ComposeApplication(this IServiceCollection services)
+        {
+            services
+                .AddContext()
+                .AddRepositories()
+                .AddMediator();
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IReadRepository<BlogPostDataModel>, BlogPostRepository>();
+            return services;
+        }
+
+        private static IServiceCollection AddContext(this IServiceCollection services)
+        {
+            services.AddScoped<AppDbContext>();
+            return services;
+        }
+
+        private static IServiceCollection AddMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(GetBlogPostsGroupedByCategory).Assembly);
+
+            return services;
+        }
+    }
+}
